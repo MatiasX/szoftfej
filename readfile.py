fd5501dcab8bb9559fd54b1d4c8ed3d41a68e80d
MrBrown16 <morabarna16@gmail.com>
2022-12-08 13:41:56 +0100

    readfile done

--- readfile.py
+++ readfile.py
@@ -6,16 +6,20 @@ class ReadFile:
 		header=file.readline() #fejléc beolvasása
 		
 		allLines = file.readlines() #sorok beolvasása
-		for a in range(len(allLines)):  #sorlista elemein végigmegy
+		for a in range(len(allLines)-1):  #sorlista elemein végigmegy
 			for i in range(0,5):  # sor szétválasztott elemein végigmegy
 				oneLine = allLines[a]  #egy sor megfogása
 				oneLineSplit=oneLine.split(":") #sor szétválasztása ":" mentén
 
-				if len(oneLine) > 1: # az utolsó, üres(csak egy whitespace) sor figyelmen kívül hagyása
+				if len(oneLineSplit) > 1: # az utolsó, üres(csak egy whitespace) sor figyelmen kívül hagyása
 					inst = oneLineSplit[i].rstrip("\n") #utolsó listaelem végéről eltávolítja a whitespace-ket (\n itt)
 					read.append(inst)
-			print("{}\t{}\t{}\t{}\t{}\t{}".format(read[0],read[1],read[2],read[3],read[4]))
+					
+			if len(read) >0:
+				print("{:20}\t{:20}\t{:20}\t{:20}\t{:20}".format(read[0],read[1],read[2],read[3],read[4]))
+			
 			read = []
 		file.close()
 
 
+	readFile()

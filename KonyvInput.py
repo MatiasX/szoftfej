98ca4c200b2c2166897c99995f55751ae20e91f4
MatiasX <36680020+MatiasX@users.noreply.github.com>
2022-12-14 14:42:26 +0100

    Kész

---
+++ KonyvInput.py
@@ -0,0 +1,21 @@
+
+
+class KonyvInput(nextline):
+
+    
+    def Konyvinput(nextline):
+        book=a + "nextline" 
+        book=Book()   
+        
+        print("Adja meg a könyv adatait!\n")
+        name=input("Könyv címe: ")
+        book.title=name
+        name=input("Írója: ")
+        book.writer=name
+        name=input("Kiadása: ")
+        book.year=name
+        name=input("Kategóriája: ")
+        book.category=name
+        name=input("Nyelve: ")
+        book.language=name
+
